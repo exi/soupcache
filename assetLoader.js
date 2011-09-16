2e2370ea7b80f0faed55f3220e69119f225b247f
Reno Reckling <e-github@wthack.de>
2011-09-16 16:55:43 +0200

    refactor assetLoader to use private variables

--- assetLoader.js
+++ assetLoader.js
@@ -7,37 +7,37 @@ var url = require('url'),
 var mod = function(options) {
     var cacheHandler = cache(options);
     var that = this;
-    that.activeDownloads = {};
-    that.callbacks = {};
-    that.downloadCount = 0;
+    var activeDownloads = {};
+    var callbacks = {};
+    var downloadCount = 0;
 
-    that.onDownloadComplete = function(url, buffer) {
-        that.downloadCount++;
+    var onDownloadComplete = function(url, buffer) {
+        downloadCount++;
         cacheHandler.insert(url, buffer);
         var mimeType = cacheHandler.getFileMimeType(url);
 
-        for (var i in that.callbacks[url]) {
-            that.callbacks[url][i](buffer, mimeType);
+        for (var i in callbacks[url]) {
+            callbacks[url][i](buffer, mimeType);
         }
 
-        delete that.callbacks[url];
-        delete that.activeDownloads[url];
+        delete callbacks[url];
+        delete activeDownloads[url];
     };
 
-    that._download = function(host, url, callback) {
-        if (!that.activeDownloads[url]) {
-            that.callbacks[url] = [callback];
-            that.activeDownloads[url] = new assetDownload(host, url, options,  that.onDownloadComplete);
+    var _download = function(host, url, callback) {
+        if (!activeDownloads[url]) {
+            callbacks[url] = [callback];
+            activeDownloads[url] = new assetDownload(host, url, options,  onDownloadComplete);
         } else {
-            that.callbacks[url].push(callback);
-            that.activeDownloads[url].addMirror(host);
+            callbacks[url].push(callback);
+            activeDownloads[url].addMirror(host);
         }
     };
 
     that.download = function(host, url, callback) {
         var buf = cacheHandler.getFileBuffer(url);
         if (buf === null) {
-            that._download(host, url, callback);
+            _download(host, url, callback);
         } else {
             callback(buf);
         }
@@ -45,14 +45,14 @@ var mod = function(options) {
 
     that.getStatus = function() {
         var status = "";
-        status += "downloaded: " + that.downloadCount;
-        if (Object.keys(that.activeDownloads).length > 0) {
+        status += "downloaded: " + downloadCount;
+        if (Object.keys(activeDownloads).length > 0) {
             status += "\n";
             status += "active downloads:" + "\n";
             var processed = 0;
-            for (var i in that.activeDownloads) {
-                var lineend = Object.keys(that.activeDownloads).length - 1 == processed?"":"\n";
-                status += that.callbacks[i].length + " clients: " + that.activeDownloads[i].getStatus() + lineend;
+            for (var i in activeDownloads) {
+                var lineend = Object.keys(activeDownloads).length - 1 == processed?"":"\n";
+                status += callbacks[i].length + " clients: " + activeDownloads[i].getStatus() + lineend;
                 processed++;
             }
         }
