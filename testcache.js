afde8dfe9ec6cb4b25b8b75c927095b8b59a00b0
Reno Reckling <e-github@wthack.de>
2011-09-16 16:37:07 +0200

    remove testcache.js

--- testcache.js
+++
@@ -1,9 +0,0 @@
-var cache = require('cache.js')({cachePath:"./cache/", loadingCachePath:"./loading/"}),
-    maxfilesize = 999999,
-    testfilename = "test.jpg";
-
-console.log(cache.exists(testfilename));
-var buf = new Buffer(maxfilesize);
-buf.write('aaaaaaaaaaaaaaaaaaaaa');
-cache.insert(testfilename, buf);
-console.log(cache.exists(testfilename));
