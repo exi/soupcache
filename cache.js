12e1e0a67401bacb7c3805cdc10bc5f1493af0ad
Reno Reckling <e-github@wthack.de>
2011-11-13 23:52:11 +0100

    add new caching scheme and catch mime errors

--- cache.js
+++ cache.js
@@ -1,39 +1,42 @@
 var fs = require('fs'),
     mime = require('mime-magic'),
+    crypto = require('crypto');
+    cacheCreated = {},
     api = function(options) {
-        var that = this;
+        var createCacheDir = function() {
+            _createCacheDir(options.cachePath);
+            _createCacheDir(options.loadingCachePath);
+        }
 
-        that.sanitizeFileName = function(filename) {
-            return encodeURIComponent(filename);
-        };
-
-        return {
-            exists: function(filename) {
-                var files = fs.readdirSync(options.cachePath);
-                for (var file in files) {
-                    if (that.sanitizeFileName(filename) == files[file]) {
-                        return true;
+        var _createCacheDir = function(dir) {
+            if (!cacheCreated[dir]) {
+                console.log("generating cache dir " + dir);
+                for (var i = 0; i<16; i++) {
+                    for (var e = 0; e<16; e++) {
+                        try  {
+                            fs.mkdirSync(dir + i.toString(16) + e.toString(16), 0755);
+                        } catch (e) {
+                            if (e.code != "EEXIST") {
+                                throw e;
+                            }
+                        }
                     }
                 }
-                return false;
-            },
-            insert: function(filename, buffer) {
-                var loadingName = options.loadingCachePath + that.sanitizeFileName(filename),
-                    cacheName = options.cachePath + that.sanitizeFileName(filename);
-                fs.writeFileSync(loadingName, buffer, 0, buffer.length, 0, 'binary');
-                fs.renameSync(loadingName, cacheName);
-            },
-            remove: function(filename) {
-                var cacheName = options.cachePath + that.sanitizeFileName(filename);
-                try {
-                    fs.unlink(cacheName);
-                } catch (e) {
-                    //file not found
-                    console.error(e);
-                }
-            },
-            getFileMimeType: function(filename, callback) {
-                var cacheName = options.cachePath + that.sanitizeFileName(filename);
+
+                cacheCreated[dir] = true;
+            }
+        }
+
+        var sanitizeFileName = function(filename) {
+            var hash = crypto.createHash("SHA1");
+            hash.update(filename);
+            var filenameHash = hash.digest(encoding="hex");
+            var hashCut = filenameHash.substr(0, 2);
+            return hashCut + "/" + encodeURIComponent(filename);
+        };
+
+        var _getFileMimeType = function(cacheName, callback, tries) {
+            try {
                 mime.fileWrapper(cacheName, function(err, type) {
                     if (err) {
                         // assume text/html
@@ -42,9 +45,46 @@ var fs = require('fs'),
                         callback(type);
                     }
                 });
+            } catch (e) {
+                if (tries < 5) {
+                    _getFileMimeType(cacheName, callback, tries + 1);
+                }
+            }
+        };
+
+        createCacheDir();
+
+        return {
+            exists: function(filename) {
+                var files = fs.readdirSync(options.cachePath);
+                for (var file in files) {
+                    if (sanitizeFileName(filename) == files[file]) {
+                        return true;
+                    }
+                }
+                return false;
+            },
+            insert: function(filename, buffer) {
+                var loadingName = options.loadingCachePath + sanitizeFileName(filename),
+                    cacheName = options.cachePath + sanitizeFileName(filename);
+                fs.writeFileSync(loadingName, buffer, 0, buffer.length, 0, 'binary');
+                fs.renameSync(loadingName, cacheName);
+            },
+            remove: function(filename) {
+                var cacheName = options.cachePath + sanitizeFileName(filename);
+                try {
+                    fs.unlink(cacheName);
+                } catch (e) {
+                    //file not found
+                    console.error(e);
+                }
+            },
+            getFileMimeType: function(filename, callback) {
+                var cacheName = options.cachePath + sanitizeFileName(filename);
+                _getFileMimeType(cacheName, callback, 0);
             },
             getFileBuffer: function(filename) {
-                var cacheName = options.cachePath + that.sanitizeFileName(filename);
+                var cacheName = options.cachePath + sanitizeFileName(filename);
                 var buffer = null;
                 try {
                     buffer = fs.readFileSync(cacheName);
