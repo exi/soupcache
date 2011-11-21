a1af6099f261fc7bbd4b2c5457941789b310452a
Reno Reckling <e-github@wthack.de>
2011-11-21 12:49:56 +0100

    improve parasoup

--- parasoupRequest.js
+++ parasoupRequest.js
@@ -1,27 +1,64 @@
 var fs = require('fs');
 var mod = function(options) {
     var clients = [],
-        getHtmlContent = function() { return fs.readFileSync("./parasoup.html", encoding='utf-8'); };
+        getHtmlContent = function() { return fs.readFileSync("./parasoup.html", encoding='utf-8'); },
+        assetCache = [],
+        stallTimer = null;
 
     options.eventBus.on('newAsset', function(url, buffer, contentType) {
         if (buffer.length > 16 * 1024) {
-            var response = "http://asset." + options.domain + url;
-            for (var i = 0; i<clients.length; i++) {
-                try{
-                    clients[i].response.writeHead(200, {
-                        'Content-Length': response.length,
-                        'Content-Type': 'text/plain'
-                    });
-                    clients[i].response.end(response);
-                    options.stats.parasoups++;
-                } catch(e) {
-                    //dont care
-                }
+            var data = {url: url};
+            if (clients.length > 0) {
+                deliverToClients(data);
+            } else {
+                assetCache.push(data);
+                options.stats.parasoupAssetCache++;
             }
-            clients = [];
+            resetStallTimer();
         }
     });
 
+    var deliverToClients = function(data) {
+        var response = "http://asset." + options.domain + data.url;
+        for (var i = 0; i<clients.length; i++) {
+            try{
+                clients[i].response.writeHead(200, {
+                    'Content-Length': response.length,
+                    'Content-Type': 'text/plain'
+                });
+                clients[i].response.end(response);
+                options.stats.parasoups++;
+            } catch (e) {
+                //dont care
+            }
+        }
+        clients = [];
+    };
+
+    var onStallTimer = function() {
+        removeStallTimer();
+
+        if (assetCache.length > 0 && clients.length > 0) {
+            var data = assetCache[0];
+            deliverToClients(data);
+            assetCache.splice(0, 1);
+            options.stats.parasoupAssetCache--;
+        } else {
+            resetStallTimer();
+        }
+    }
+
+    var removeStallTimer = function() {
+        if (stallTimer !== null) {
+            clearTimeout(stallTimer);
+            stallTimer = null;
+        }
+    }
+
+    var resetStallTimer = function() {
+        removeStallTimer();
+        stallTimer = setTimeout(onStallTimer, 2000);
+    }
 
     return function(request, response) {
         if (request.url == "/newStuff") {
--- soupcache.js
+++ soupcache.js
@@ -21,7 +21,7 @@ var url = require('url'),
     };
 
 options.assetLoader = new assetLoader(options);
-options.stats = { dataCount: {}, redirects: 0, parasoups: 0 };
+options.stats = { dataCount: {}, redirects: 0, parasoups: 0, parasoupAssetCache: 0 };
 options.eventBus = new events.EventEmitter();
 http.globalAgent.maxSockets = 50;
 
@@ -99,6 +99,7 @@ statusProvider.push(function() {
     var status = "";
 
     status += "parasoups served: " + options.stats.parasoups + "\n";
+    status += "parasoup asset cache: " + options.stats.parasoupAssetCache + "\n";
     status += dataArray.length + " clients seen\n";
     status += "Top " + maxLines + " users:\n";
 
