4ae65cfcba072aa08be185bb641bf155ceae8d95
Reno Reckling <reno.reckling@mayflower.de>
2012-07-15 12:26:02 +0200

    update stats

--- soupcache.js
+++ soupcache.js
@@ -78,51 +78,46 @@ var startupComponents = function(options) {
     statusProvider.push(options.assetLoader.getStatus);
 
     statusProvider.push(function() {
-        var maxLines = 10;
-        var convertToHumanReadable = function(bytes) {
-            var factors = [[1, 'B'], [1024, 'KB'], [1048576, 'MB'], [1073741824, 'GB']];
-            var ret = "";
-            for (var i = 0; i < factors.length; i++) {
-                if (i + 1 >= factors.length || (bytes > factors[i][0] && bytes < factors[i + 1][0]) || bytes === 0) {
-                    ret = Math.floor(bytes / (factors[i][0])) + factors[i][1];
-                    break;
+        var start = new Date();
+        return function() {
+            var maxLines = 10;
+            var convertToHumanReadable = function(bytes) {
+                var factors = [[1, 'B'], [1024, 'KB'], [1048576, 'MB'], [1073741824, 'GB']];
+                var ret = "";
+                for (var i = 0; i < factors.length; i++) {
+                    if (i + 1 >= factors.length || (bytes > factors[i][0] && bytes < factors[i + 1][0]) || bytes === 0) {
+                        ret = Math.floor(bytes / (factors[i][0])) + factors[i][1];
+                        break;
+                    }
+                }
+                return ret;
+            };
+
+            var sumBytes = 0;
+
+            var dataArray = options.stats.dataCount;
+            for (var i in options.stats.dataCount) {
+                if (!(i === "" || typeof i == "undefined" || isNaN(dataArray[i]))) {
+                    sumBytes += dataArray[i];
                 }
             }
-            return ret;
+
+            var now = new Date();
+            var diff = (now - start) / 1000 / 60;
+            var served = ( options.stats.parasoups || 0 );
+            var spm = Math.floor(( served / diff ) * 10) / 10;
+            var bpm = Math.floor(( sumBytes / diff ) * 10) / 10;
+
+            var status = "";
+
+            status += "total data served: " + convertToHumanReadable(sumBytes) + " " + convertToHumanReadable(bpm) + "/min\n";
+            status += "assets on server: " + options.stats.assetCount + "\n";
+            status += "parasoups served: " + served + " " + spm + "/min\n";
+            status += "parasoup asset cache: " + options.stats.parasoupAssetCache + "\n";
+
+            return status;
         };
-
-        var dataArray = [];
-
-        for (var i in options.stats.dataCount) {
-            dataArray.push(i);
-        }
-
-        for (var i = 0; i < dataArray.length; i++) {
-            if (dataArray[i] === "" || typeof dataArray[i] == "undefined" || isNaN(options.stats.dataCount[dataArray[i]])) {
-                delete options.stats.dataCount[dataArray[i]];
-                dataArray.splice(i, 1);
-            }
-        }
-
-        var byteSort = function(a, b) {
-            return parseInt(options.stats.dataCount[b]) - parseInt(options.stats.dataCount[a]);
-        };
-
-        var anonymizeIP = function(ip) {
-            var lastDot = ip.lastIndexOf(".");
-            return ip.substr(0, lastDot) + "\\.*";
-        };
-
-        dataArray.sort(byteSort);
-
-        var status = "";
-
-        status += "assets on server: " + options.stats.assetCount + "\n";
-        status += "parasoups served: " + options.stats.parasoups + "\n";
-        status += "parasoup asset cache: " + options.stats.parasoupAssetCache + "\n";
-
-        return status;
-    });
+    } ());
 
     options.statPrinter = new statPrinter(statusProvider);
 
