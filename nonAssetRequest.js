ea30f40bb79d38150b3a543a8d6d7c6e9ff8194a
Reno Reckling <reno.reckling@mayflower.de>
2012-07-23 10:47:12 +0200

    fix disclaimer size

--- nonAssetRequest.js
+++ nonAssetRequest.js
@@ -156,7 +156,7 @@ var mod = function(options) {
 
             var insertProxyNote = function(inputdata) {
                 return inputdata.
-                    replace(/<ul id="menu">/g, '<ul id="menu" style="padding-left: 0px !important; margin-left: -200px !important;"><li style="background-color: red; font-size: 18pt; color: black;"><div style="padding: 8px 14px;">THIS IS A PROXY NOT THE REAL SOUP</div></li>');
+                    replace(/<ul id="menu">/g, '<ul id="menu" style="padding-left: 0px !important;"><li style="background-color: red; font-size: 14px; color: black;"><div style="padding: 8px 14px;">THIS IS A PROXY NOT THE REAL SOUP</div></li>');
             }
 
             var modifyHtmlAndReturnBuffer = function(inputdata) {
